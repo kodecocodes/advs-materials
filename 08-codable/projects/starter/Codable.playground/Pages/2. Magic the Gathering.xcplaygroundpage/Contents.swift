9a2d72783c92f2209cbb03a2445b32f463a19303
freak4pc <freak4pc@gmail.com>
2020-08-10 21:55:28 +0300

    Update starter for MTG

--- 08-codable/projects/starter/Codable.playground/Pages/2. Magic the Gathering.xcplaygroundpage/Contents.swift
+++ 08-codable/projects/starter/Codable.playground/Pages/2. Magic the Gathering.xcplaygroundpage/Contents.swift
@@ -2,4 +2,26 @@
 
 import UIKit
 
+let data = API.getData(for: .magicTheGathering)
+
+let decoder = JSONDecoder()
+do {
+  let cards = try decoder.decode([Card].self, from: data)
+  for card in cards {
+    print("🃏 \(card.name) #\(card.number)")
+  }
+} catch {
+  print("Something went wrong: \(error)")
+}
+
+struct Card: Decodable {
+  let id: String
+  let name: String
+  let type: String
+  let text: String
+  let number: String
+  let flavor: String?
+  let imageUrl: URL?
+}
+
 //: [Next](@next)
