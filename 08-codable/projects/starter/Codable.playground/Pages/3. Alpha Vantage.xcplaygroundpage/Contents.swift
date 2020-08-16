0cf2630d82f15a3fcc28a76e1e22a568a72d6c56
freak4pc <freak4pc@gmail.com>
2020-08-16 12:54:51 +0300

    AV Clean Starter

--- 08-codable/projects/starter/Codable.playground/Pages/3. Alpha Vantage.xcplaygroundpage/Contents.swift
+++ 08-codable/projects/starter/Codable.playground/Pages/3. Alpha Vantage.xcplaygroundpage/Contents.swift
@@ -3,4 +3,14 @@
 import Foundation
 import PlaygroundSupport
 
+func getStock(interval: API.AlphaVantageInterval) throws -> Stock {
+  let data = API.getData(for: .alphaVantage(interval: interval))
+  let decoder = JSONDecoder()
+
+  return try decoder.decode(Stock.self, from: data)
+}
+
+struct Stock: Decodable {
+
+}
 //: [Next](@next)
