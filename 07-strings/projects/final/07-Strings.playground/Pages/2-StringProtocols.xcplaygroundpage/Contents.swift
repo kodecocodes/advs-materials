a4b9cf15152fd69778deb0e3c4f964947d005298
Ehab Yosry Amer <ehab.amer@gmail.com>
2020-08-29 19:35:07 +0200

    progress

---
+++ 07-strings/projects/final/07-Strings.playground/Pages/2-StringProtocols.xcplaygroundpage/Contents.swift
@@ -0,0 +1,70 @@
+/// Copyright (c) 2020 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import Foundation
+
+var sampleString = "Lo͞r̉em̗ ȉp͇sum̗ do͞l͙o͞r̉ sȉt̕ a͌m̗et̕"
+
+sampleString.last
+let reversedString = String(sampleString.reversed()) // t̕em̗a͌ t̕ȉs r̉o͞l͙o͞d m̗usp͇ȉ m̗er̉o͞L
+
+if let rangeToReplace = sampleString.range(of: "Lo͞r̉em̗") {
+    sampleString.replaceSubrange(rangeToReplace, with: "Lorem") // Lorem ȉp͇sum̗ do͞l͙o͞r̉ sȉt̕ a͌m̗et̕
+}
+
+extension String {
+    subscript(position: Int) -> Self.Element {
+        get {
+            let characters = Array(self)
+            return characters[position]
+        }
+        set(newValue) {
+            let startIndex = self.index(self.startIndex, offsetBy: position)
+            let endIndex = self.index(self.startIndex, offsetBy: position + 1)
+            let range = startIndex..<endIndex
+            replaceSubrange(range, with: [newValue])
+        }
+    }
+}
+
+sampleString[2]
+sampleString[2] = "R"
+
+sampleString
+
+for i in 0..<sampleString.count {
+    sampleString[i].uppercased()
+}
+
+for element in sampleString {
+    element.uppercased()
+}
