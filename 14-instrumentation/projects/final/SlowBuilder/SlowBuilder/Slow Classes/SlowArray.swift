64bfb29d6771cb8341d2e5bfb62b9c5807aa68c7
Ehab Yosry Amer <ehab.amer@gmail.com>
2020-11-03 11:10:46 +0200

    updated printArray() in final project

--- 14-instrumentation/projects/final/SlowBuilder/SlowBuilder/Slow Classes/SlowArray.swift
+++ 14-instrumentation/projects/final/SlowBuilder/SlowBuilder/Slow Classes/SlowArray.swift
@@ -6,10 +6,10 @@ import Foundation
 
 class SlowArray {
   func printArray() {
-    let doubleArray = Array(repeating: 1.123, count: 100)
-      .map { $0 * Double.random(in: 0 ..< 1000) }
-    let intArray = Array(repeating: 1, count: 100)
-      .map { $0 * Int.random(in: 0 ..< 1000) }
+    var doubleArray = Array(repeating: 1.123, count: 100)
+    doubleArray = doubleArray.map { $0 * Double.random(in: 0 ..< 1000) }
+    var intArray = Array(repeating: 1, count: 100)
+    intArray = intArray.map { $0 * Int.random(in: 0 ..< 1000) }
     let doubleMultiply = zip(doubleArray, intArray)
       .map { $0 * Double($1) }
     let sum = doubleMultiply.sorted()
