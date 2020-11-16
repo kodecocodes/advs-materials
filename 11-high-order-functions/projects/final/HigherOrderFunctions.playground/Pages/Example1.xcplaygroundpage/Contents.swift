d6f27d8ff25b57fd48d970f5f24e01db9f997365
Ehab Yosry Amer <ehab.amer@gmail.com>
2020-11-16 12:11:55 +0200

    getting started

---
+++ 11-high-order-functions/projects/final/HigherOrderFunctions.playground/Pages/Example1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,45 @@
+/// Sample code from the book, Advanced Swift, published at raywenderlich.com, Copyright 2020.
+/// See LICENSE for details. Thank you for supporting our work!
+/// Visit https://www.raywenderlich.com/books/advanced-swift
+
+class TextPrinter {
+  var formatter: ParagraphFormatterProtocol
+
+  init(formatter: ParagraphFormatterProtocol) {
+    self.formatter = formatter
+  }
+
+  func printText(_ paragraphs: [String]) {
+    for text in paragraphs {
+      let formattedText = formatter.formatParagraph(text)
+      print(formattedText)
+    }
+  }
+}
+
+protocol ParagraphFormatterProtocol {
+  func formatParagraph(_ text: String) -> String
+}
+
+class SimpleFormatter: ParagraphFormatterProtocol {
+  func formatParagraph(_ text: String) -> String {
+    guard !text.isEmpty else { return text }
+    var formattedText = text.prefix(1).uppercased() + text.dropFirst()
+    if let lastCharacter = formattedText.last,
+       !lastCharacter.isPunctuation {
+      formattedText += "."
+    }
+    return formattedText
+  }
+}
+
+let simpleFormatter = SimpleFormatter()
+let textPrinter = TextPrinter(formatter: simpleFormatter)
+
+let exampleParagraphs = [
+  "basic text example",
+  "Another text example!!",
+  "one more text example"
+]
+
+textPrinter.printText(exampleParagraphs)
