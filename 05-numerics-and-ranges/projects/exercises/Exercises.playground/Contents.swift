d514f54935e16788218100764f18dd6bdf184a8a
Ray Fix <rayfix@gmail.com>
2020-11-18 13:51:29 -0800

    Fix build, add shorter LICENSE comment and sprinkle LICENSES everywhere.

--- 05-numerics-and-ranges/projects/exercises/Exercises.playground/Contents.swift
+++ 05-numerics-and-ranges/projects/exercises/Exercises.playground/Contents.swift
@@ -1,3 +1,7 @@
+/// Sample code from the book, Advanced Swift,
+/// published at raywenderlich.com, Copyright (c) 2020 Razeware LLC.
+/// See LICENSE for details. Thank you for supporting our work!
+/// Visit https://www.raywenderlich.com/books/advanced-swift
 /*
  - What are the minimum and maximum representable values of a make-believe `Int4` and `Int10` type?
 
