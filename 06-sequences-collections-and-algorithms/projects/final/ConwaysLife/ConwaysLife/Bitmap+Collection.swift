c5202345cffb3c3b0f6a27fa0f96a006df3f4b5c
Ray Fix <rayfix@gmail.com>
2020-12-15 20:57:40 -0800

    Final project for ConwaysLife

---
+++ 06-sequences-collections-and-algorithms/projects/final/ConwaysLife/ConwaysLife/Bitmap+Collection.swift
@@ -0,0 +1,77 @@
+/// Sample code from the book, Advanced Swift,
+/// published at raywenderlich.com, Copyright (c) 2020 Razeware LLC.
+/// See LICENSE for details. Thank you for supporting our work!
+/// Visit https://www.raywenderlich.com/books/advanced-swift
+
+/// Make Bitmap conform to some useful collection protocols.
+extension Bitmap: RandomAccessCollection, MutableCollection {
+  /// Index stores row, column locations of a Bitmap.  It assumes row-major order
+  /// where row is considered before column.
+  @usableFromInline
+  struct Index: Comparable {
+    static func < (lhs: Index, rhs: Index) -> Bool {
+      (lhs.row, lhs.column) < (rhs.row, lhs.column)
+    }
+    var row, column: Int
+  }
+
+  /// Start of the Bitmap is the upper-left corner.
+  @inlinable var startIndex: Index {
+    Index(row: 0, column: 0)
+  }
+
+  /// Forward traversal takes place in raster-order.
+  @inlinable var endIndex: Index {
+    Index(row: height, column: 0)
+  }
+
+  /// Forward traversal required by collections. Raster-scan order.
+  @inlinable func index(after i: Index) -> Index {
+    i.column < width-1 ?
+      Index(row: i.row, column: i.column+1) :
+      Index(row: i.row+1, column: 0)
+  }
+
+  /// Reverse traversal required by bidirectional collections.  Raster-scan order.
+  @inlinable func index(before i: Index) -> Index {
+    i.column > 0 ?
+      Index(row: i.row,   column: i.column-1) :
+      Index(row: i.row-1, column: width-1)
+  }
+  ///　Requred by RandomAccessCollection.  Advance in raster-scan order.
+  @inlinable func index(_ i: Index, offsetBy distance: Int) -> Index {
+    Index(row: i.row+distance/width, column: i.column+distance%width)
+  }
+
+  /// Distance between two points in raster-scan order.
+  @inlinable func distance(from start: Index, to end: Index) -> Int {
+    (end.row*width+end.column) - (start.row*width+start.row)
+  }
+
+  /// Convenience method.  Create an index by offsettng the row and column.
+  /// The result index is not necessarily contained in the collection.
+  @inlinable func index(of i: Index, rowOffset: Int, columnOffset: Int) -> Index {
+    Index(row: i.row + rowOffset, column: i.column + columnOffset)
+  }
+
+  /// Convenience method for determining if an index is in bounds.
+  @inlinable func contains(index: Index) -> Bool {
+    return (0..<width).contains(index.column) && (0..<height).contains(index.row)
+  }
+
+  @inlinable subscript(position: Index) -> Pixel {
+
+    /// Required by collections.
+    get {
+      precondition(contains(index: position), "out of bounds index \(position)")
+      return pixels[position.row*width+position.column]
+    }
+
+    /// MutableCollection magic.  It does not invalidate indicies
+    set {
+      precondition(contains(index: position), "out of bounds index \(position)")
+      pixels[position.row*width+position.column] = newValue
+    }
+  }
+}
+
---
+++ 06-sequences-collections-and-algorithms/projects/final/ConwaysLife/ConwaysLife/RingMemory.swift
@@ -0,0 +1,38 @@
+/// Sample code from the book, Advanced Swift,
+/// published at raywenderlich.com, Copyright (c) 2020 Razeware LLC.
+/// See LICENSE for details. Thank you for supporting our work!
+/// Visit https://www.raywenderlich.com/books/advanced-swift
+
+import Foundation
+
+/// A simple abstraction for keeping a ring buffer of equatable  values.
+/// You can check to see if one of the values was added, but the memory.
+/// It only remembers the number of values
+struct RingMemory<Element: Equatable> {
+  private var elements: [Element?]
+  private var addIndex: Int = 0
+
+  /// Initialize RingMemory with capacity that specifies how many objects
+  /// to remember.
+  init(capacity: Int) {
+    precondition(capacity > 0, "capacity must be greater than zero")
+    elements = Array(repeating: nil, count: capacity)
+  }
+
+  /// Returns true if the element has been seen recently (specified by capacity)
+  func contains(_ element: Element) -> Bool {
+    elements.first { $0 == element } != nil
+  }
+
+  /// Adds a new element, potentially forgets an older one, controlled by capacity.
+  mutating func add(_ element: Element) {
+    elements[addIndex] = element
+    addIndex = (addIndex+1) % elements.count
+  }
+
+  /// Resets what has been seen.
+  mutating func reset() {
+    elements = Array(repeating: nil, count: elements.count)
+    addIndex = 0
+  }
+}
