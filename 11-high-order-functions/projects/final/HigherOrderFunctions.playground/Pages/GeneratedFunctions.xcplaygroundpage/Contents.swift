ee30a5fbd9834a368d426dd83b731cc66db1e26f
Ehab Yosry Amer <ehab.amer@gmail.com>
2021-02-13 19:43:17 +0200

    Material updates

---
+++ 11-high-order-functions/projects/final/HigherOrderFunctions.playground/Pages/GeneratedFunctions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func flip<A, C>(
+  _ originalMethod: @escaping (A) -> () -> C
+) -> () -> (A) -> C {
+  return { { a in originalMethod(a)() } }
+}
+
+extension Int {
+  func word() -> String? {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .spellOut
+    return formatter.string(from: self as NSNumber)
+  }
+}
+
+1.word() // one
+10.word() // ten
+36.word() // thirty-six
+
+Int.word // (Int) -> () -> Optional<String>
+
+Int.word(1)() // one
+Int.word(10)() // ten
+Int.word(36)() // thirty-six
+
+flip(Int.word)()(1) // one
+
+var flippedWord = flip(Int.word)()
+
+[1, 2, 3, 4, 5].map(
+  flippedWord
+)
+
+func reduce<A, C>(
+  _ originalMethod: @escaping (A) -> () -> C
+) -> (A) -> C {
+  return { a in originalMethod(a)() }
+}
+
+var reducedWord = reduce(Int.word)
