f6c4ee4d17ed820ee1af0886b9e0f0c533bdc140
Ray Fix <rayfix@gmail.com>
2021-02-17 23:25:32 -0800

    Bump copyright date to 2021

--- 06-sequences-collections-and-algorithms/projects/final/ConwaysLife/ConwaysLife/ContentView.swift
+++ 06-sequences-collections-and-algorithms/projects/final/ConwaysLife/ConwaysLife/ContentView.swift
@@ -1,5 +1,5 @@
 /// Sample code from the book, Advanced Swift,
-/// published at raywenderlich.com, Copyright (c) 2020 Razeware LLC.
+/// published at raywenderlich.com, Copyright (c) 2021 Razeware LLC.
 /// See LICENSE for details. Thank you for supporting our work!
 /// Visit https://www.raywenderlich.com/books/advanced-swift
 
