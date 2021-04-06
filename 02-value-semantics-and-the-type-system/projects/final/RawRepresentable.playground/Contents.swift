7e2094a560172933d81c20e55c1d597cdccbb96e
Ray Fix <rayfix@gmail.com>
2021-04-06 00:26:13 -0700

    Add RawRepresentable

---
+++ 02-value-semantics-and-the-type-system/projects/final/RawRepresentable.playground/Contents.swift
@@ -0,0 +1,28 @@
+/// Sample code from the book, Advanced Swift,
+/// published at raywenderlich.com, Copyright (c) 2021 Razeware LLC.
+/// See LICENSE for details. Thank you for supporting our work!
+/// Visit https://www.raywenderlich.com/books/advanced-swift
+
+enum Coin: Int, CaseIterable {
+  case penny = 1, nickel = 5, dime = 10, quarter = 25
+}
+
+let lucky = Coin(rawValue: 1)
+lucky?.rawValue
+
+struct Email: RawRepresentable {
+  var rawValue: String
+
+  init?(rawValue: String) {
+    guard rawValue.contains("@") else {
+      return nil
+    }
+    self.rawValue = rawValue
+  }
+}
+
+func send(message: String, to: Email) {
+
+}
+
+
